7dae7e9b22be1da78528ceb68b197eed5d40b0d3
siddhi-gargs <gargsiddhi2005@gmail.com>
2024-11-26 13:23:03 +0530

    Update Guess_the_number_InRange.js

--- Guess_the_number_InRange.js
+++ Guess_the_number_InRange.js
@@ -1,102 +1,98 @@
 // Assume rangeStart and rangeEnd are always greater than 0.
 // rangeStart is always less than rangeEnd.
 
-function noOfDigit(number) {
-  if (number < 10) {
-    return 1;
-  }
-
-  return 1 + noOfDigit(number / 10);
-}
-
 function oneLineSpace() {
   console.log();
 }
 
-function generateNumber(a) {
-  const b = Math.ceil(Math.random() * (10 ** a));
-  return b;
+function generateNumber(from, to) {
+  return from + Math.floor(Math.random() * (to - from));
 }
 
 function winningMsg() {
-  return "you guess the correct number 😎👍👏";
+  console.log("you guess the correct number 😎👍👏");
 }
 
 function msgForEndTheGame(randomNumber) {
-  console.log("Number was : " + randomNumber);
-  return "your all attempts are finished";
+  oneLineSpace();
+  console.log("Number was : ", randomNumber);
+  console.log("You lost your all Attempts");
 }
 
-function areEqual(random, input) {
+function isUserGuessCorrect(random, input) {
   return random === input;
 }
 
-function messageForNavigatingRemindingAttempt(attempt) {
-  return "you have remaining " + attempt + " no of attempts";
+function remainingAttempts(attempt) {
+  console.log("you have remaining " + attempt + " no of attempts");
 }
 
-function isInRange(start, end, number) {
-  return number > start && end > number;
+function isInRange(start, end, userInput) {
+  return userInput >= start && end > userInput ;
 }
 
 function clue(input, random) {
-
+  oneLineSpace();
   if (input > random) {
     return "Guess lesser number🙄";
   }
   return "Guess greater number👀";
 }
 
-function welComeMessage(rangeStart, rangeEnd) {
-  oneLineSpace();
-  console.log("------- This is GUESS THE RIGHT NUMBER GAME -----------");
-  oneLineSpace();
-  console.log("Your Range start with " + rangeStart + " and End with " + rangeEnd);
-  oneLineSpace();
-}
-
-function isUserInputValid(start, end, totalAttempts) {
+function getUserInput(start, end) {
   oneLineSpace();
   const userInput = prompt("guess the number :");
-  const isUserInputInRange = isInRange(start, end, +userInput);
-  if (!isUserInputInRange) {
+  
+  if (!isInRange(start, end, +userInput)) {
     oneLineSpace();
     console.log("Invalid input");
     oneLineSpace();
-    console.log(messageForNavigatingRemindingAttempt(totalAttempts));
-    return isUserInputValid(start, end, totalAttempts);
+    return getUserInput(start, end);
   } 
   return +userInput;
 }
 
-function evaluateRemainAttempts(start, end, totalAttempts, randomNumber) {
-  if (totalAttempts === 0) {
-    return console.log(msgForEndTheGame(randomNumber));
-  }
-
-  console.log(messageForNavigatingRemindingAttempt(totalAttempts));
-
-  const userInput = isUserInputValid(start, end, totalAttempts);
+function displayInstruction(rangeStart, rangeEnd, attempts) {
   oneLineSpace();
-  const isEnteredNumCorrect = areEqual(randomNumber, userInput);
-  
-  if (isEnteredNumCorrect) {
-    return console.log(winningMsg());
-  }
-  console.log(clue(userInput, randomNumber));
+  console.log("You have to Guess number between ", rangeStart, "and", rangeEnd);
   oneLineSpace();
-  return evaluateRemainAttempts(start, end, totalAttempts - 1, randomNumber);
+  console.log("You will got Total", attempts, "attempts");
 }
 
-function startGame(rangeStart, rangeEnd, maxAttempts) {
-  const noOfDigitInStartRange = noOfDigit(rangeEnd - 1);
-  const randomNumber = generateNumber(noOfDigitInStartRange);
+function mainFunc(rangeStart, rangeEnd, maxAttempts) {
+  let attempts = maxAttempts;
+  const randomNumber = generateNumber(rangeStart, rangeEnd);  
+  displayInstruction(rangeStart, rangeEnd, attempts);
+
+  while (attempts > 0) {
+    const userInput = getUserInput(rangeStart, rangeEnd);
+    if (isUserGuessCorrect(userInput, randomNumber)) {
+      winningMsg();
+      return askAgainForPlaying();
+    }
+    attempts--;
+    if (attempts > 0) {
+      console.log(clue(userInput, randomNumber));
+      oneLineSpace();
+      remainingAttempts(attempts);
+    }
+  }
   
-  if (isInRange(rangeStart, rangeEnd, randomNumber)) {
-    welComeMessage(rangeStart, rangeEnd);
-    return evaluateRemainAttempts(rangeStart, rangeEnd, maxAttempts, randomNumber);
-  } 
-  return startGame(rangeStart, rangeEnd, maxAttempts);
+  msgForEndTheGame(randomNumber);
+  return askAgainForPlaying();
 }
 
-startGame(10, 100, 6);
+function askAgainForPlaying() {
+  const getInput = confirm("Do you want to play this Game Again..?");
+  return getInput ? mainFunc(50, 200, 7) : "Thanks for Your Time";
+}
+
+function startGame(rangeStart, rangeEnd, attempt) {
+  oneLineSpace();
+  console.log("------- This is GUESS THE RIGHT NUMBER GAME -----------");
+  oneLineSpace();
+  const wantToPlay = confirm("Do you want to play This GAME...?");
+  return wantToPlay ? mainFunc(rangeStart, rangeEnd, attempt) : "Your choice";  
+}
+
+console.log(startGame(10, 100, 3));
